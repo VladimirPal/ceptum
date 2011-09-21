3d7c297690eefb3659fd416cf0f54364a373d41e
Владимир <vladimir@MacBook-Pro-Vladimir.local>
2011-09-21 17:21:49 +0400

    New store

--- cart/settings.py
+++ cart/settings.py
@@ -6,8 +6,8 @@ DISCOUNT_PERCENT = 10
 COUNT_FOR_DISCOUNT = 2
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'info@my-spy.ru'
-EMAIL_HOST_PASSWORD = 'kjaH71baHGas'
+EMAIL_HOST_USER = 'info@cctvision.ru
+EMAIL_HOST_PASSWORD = 'k8281asjKasjdhg'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
