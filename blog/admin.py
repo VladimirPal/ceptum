c33320de1cfadd91361ea28a556739aabdfc3cfc
Владимир <vladimir@MacBook-Pro-Vladimir.local>
2011-10-26 17:51:38 +0400

    Bug fix

--- blog/admin.py
+++ blog/admin.py
@@ -4,6 +4,8 @@ from blog.models import Entry, Category
 class EntrysAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug' : ('title',)}
     list_display = ('title', 'date',)
+    class Media:
+        js = ['/static/grappelli/tinymce/jscripts/tiny_mce/tiny_mce.js', '/static/grappelli/tinymce_setup/tinymce_setup.js',]
 
 class CategoriesAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug' : ('name',)}
