b45e33bcc13dc2cb502a1d79d0bc4cbd98885708
Владимир <vladimir@MacBook-Pro-Vladimir.local>
2011-12-06 15:41:14 +0400

    Bug fix

--- blog/feeds.py
+++ blog/feeds.py
@@ -2,8 +2,8 @@ from django.contrib.syndication.views import Feed
 from blog.models import Entry
 
 class RssSiteNewsFeed(Feed):
-    title = "CCTVision Blog"
-    link = "cctvision.ru/blog/rss/"
+    title = "Ceptum Blog"
+    link = "ceptum.ru/blog/rss/"
 
     def items(self):
         return Entry.objects.order_by('-date')[:5]
