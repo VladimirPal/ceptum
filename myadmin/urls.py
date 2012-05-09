616a9abf3416a99b80538c5e12775e171a44987c
cheguevara <mail@kvove.ru>
2012-05-09 21:48:28 +0400

    Bug fix

--- myadmin/urls.py
+++ myadmin/urls.py
@@ -5,5 +5,5 @@ urlpatterns = patterns('',
                       url(r'^logout/$', 'myadmin.views.logout_view', name="logout-page"),
                       url(r'^clients/$', 'myadmin.views.clients', name="clients"),
                       url(r'^clients/add$', 'myadmin.views.add_client', name="add_client"),
-                      url(r'^client/edit-status$', 'myadmin.views.edit_status', name="edit_status"),
-                      url(r'^client/edit/(?P<id>[-\w]+)/$', 'myadmin.views.edit_client', name="edit-client"),)
+                      url(r'^clients/edit-status$', 'myadmin.views.edit_status', name="edit_status"),
+                      url(r'^clients/edit/(?P<id>[-\w]+)/$', 'myadmin.views.edit_client', name="edit-client"),)
