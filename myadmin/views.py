6bd0d350de7d8de37cf4d90a6c9c6db2eb3da649
cheguevara <mail@kvove.ru>
2012-05-12 13:06:26 +0400

    Bug fix

--- myadmin/views.py
+++ myadmin/views.py
@@ -57,7 +57,9 @@ def clients(request):
             clients = Client.objects.filter(user=user, status__in=current_statuses).order_by('status_date')
     except :
         clients = False
-    expired_count = Client.objects.filter(user=user).exclude(status_date__gt=datetime.date.today()).count()
+    expired_client = Client.objects.filter(user=user).exclude(status_date__gt=datetime.date.today())
+    expired_count = expired_client.count()
+    print expired_count
     today_count = Client.objects.filter(user=user, status_date=datetime.date.today()).count()
     status_statistic = {}
     for status, y in STATUS_CHOICES:
