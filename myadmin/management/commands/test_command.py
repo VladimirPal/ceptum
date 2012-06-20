f4a6f6174f9c455012e2303cf326fb6d416a6f0c
cheguevara <mail@kvove.ru>
2012-06-20 13:42:19 +0400

    Bug fix

--- myadmin/management/commands/test_command.py
+++ myadmin/management/commands/test_command.py
@@ -1,34 +1,13 @@
           # -*- coding: utf-8 -*-
 from django.core.management.base import BaseCommand
-from catalog.models import Product, Category, CameraProduct, CategoryProduct, Feature
+from myadmin.models import Target, Phone
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        category = Category.objects.get(slug='indoor')
-        products = category.product_set.all()
-
-        for product in products:
-            camproduct = CameraProduct()
-            camproduct.name = product.name
-            camproduct.slug = product.slug
-            camproduct.price = product.price
-            camproduct.wholesale_price = product.wholesale_price
-            camproduct.quantity = product.quantity
-            camproduct.mini_html_description = product.mini_html_description
-            camproduct.html_description = product.html_description
-            camproduct.tech_details = product.tech_details
-            camproduct.thumbnail_image = product.thumbnail_image
-            camproduct.is_active = product.is_active
-            camproduct.in_stock = product.in_stock
-            camproduct.is_discount = product.is_discount
-            camproduct.created_at = product.created_at
-            camproduct.updated_at = product.updated_at
-            all_cats = list(product.categoryproduct_set.all())
-            product.delete()
-            camproduct.save()
-            for i in all_cats:
-                m1 = CategoryProduct(category = i.category, product = camproduct, sort_number = i.sort_number)
-                m1.save()
-            camproduct.save()
-#            product.delete()
-
+        targets = Target.objects.filter(category_id=1)
+        for target in targets:
+            try:
+                Phone.objects.get(target=target).delete()
+            except :
+                pass
+            target.delete()
